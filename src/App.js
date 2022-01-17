fccfaa8c4e431d2a37faa479836e34898299fb5c
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-01-17 17:18:16 -0500

    Style footer so it's on the bottom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./style/Reset.css";
+import "./style/App.css"
 import Header from "./components/Header";
 import Meme from "./components/Meme";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Meme />
+    <div className="app-container">
+      <div className="non-footer-content">
+        <Header />
+        <Meme />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
