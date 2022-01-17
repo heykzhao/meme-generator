ab0d85e2626c4b596a738c0719d8ded420de99db
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-01-17 17:25:03 -0500

    Move footer to below header to prevent sticky footer issue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import "./style/Reset.css";
-import "./style/App.css"
 import Header from "./components/Header";
 import Meme from "./components/Meme";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="app-container">
-      <div className="non-footer-content">
-        <Header />
-        <Meme />
-      </div>
-      <div className="footer">
-        <Footer />
-      </div>
+    <div>
+      <Header />
+      <Footer />
+      <Meme />
     </div>
   );
 }
