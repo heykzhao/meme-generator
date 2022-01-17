dfdc796166401f67f390b881bcbefb5e4e864890
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-01-17 17:17:59 -0500

    Add sub-header describing app

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,14 @@ import trollFaceLogo from "../images/troll-face.png";
 function Header() {
   return (
       <header className="header">
-        <img 
-          alt="Troll face logo"
-          src={trollFaceLogo}
-        />
-        <h2>Meme Generator</h2>
+        <div className="header-logo">
+          <img 
+            alt="Troll face logo"
+            src={trollFaceLogo}
+          />
+          <h2>Meme Generator</h2>
+        </div>
+        <h4>Randomly generated memes, draggable text.</h4>
       </header>
   )
 }
